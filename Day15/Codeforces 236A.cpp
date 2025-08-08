571a361a0c92e92d7c83f3834ec79a048055bd1e
Neel Pandey <neelpandeyofficial@gmail.com>
2025-08-08 15:29:23 +0530

    DAY15 DONE!

---
+++ Day15/Codeforces 236A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <set>
+using namespace std;
+int main(){
+    set<char> distinct;
+    string name;
+    cin>>name;
+    for(char i:name){
+        distinct.insert(i);
+    }
+    if(distinct.size()%2==0){
+        cout<<"CHAT WITH HER!";
+    }else{
+        cout<<"IGNORE HIM!";
+    }
+    return 0;
+}
