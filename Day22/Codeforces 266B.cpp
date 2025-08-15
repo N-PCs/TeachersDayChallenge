5506543b31ebdba8e23f04410beda35ad1e0fcbf
Neel Pandey <neelpandeyofficial@gmail.com>
2025-08-15 23:35:59 +0530

    DAY22 DONE!

---
+++ Day22/Codeforces 266B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+    int n, t;
+    string s;
+    cin >> n >> t >> s;
+    
+    for (int time = 0; time < t; ++time) {
+        string next = s;
+        for (int i = 0; i < n - 1; ++i) {
+            if (s[i] == 'B' && s[i+1] == 'G') {
+                next[i] = 'G';
+                next[i+1] = 'B';
+            }
+        }
+        s = next;
+    }
+    cout << s << endl;
+    return 0;
+}
