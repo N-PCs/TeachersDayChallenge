b708928c828c826613838985f10d0f089583df14
Neel Pandey <neelpandeyofficial@gmail.com>
2025-09-26 22:07:15 +0530

    Update Codeforces 236A.cpp

--- Day15/Codeforces 236A.cpp
+++ Day15/Codeforces 236A.cpp
@@ -2,16 +2,21 @@
 #include <set>
 using namespace std;
 int main(){
+    
     set<char> distinct;
+    
     string name;
     cin>>name;
+    
     for(char i:name){
         distinct.insert(i);
     }
+    
     if(distinct.size()%2==0){
         cout<<"CHAT WITH HER!";
     }else{
         cout<<"IGNORE HIM!";
     }
+    
     return 0;
 }
